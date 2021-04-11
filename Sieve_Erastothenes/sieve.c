8ccc389b3e1305bf611fa13145b3659496ca4dd8
omennia <ianginthnemo@gmail.com>
2021-04-11 17:21:25 +0100

    change in sieve

--- Sieve_Erastothenes/sieve.c
+++ Sieve_Erastothenes/sieve.c
@@ -12,13 +12,13 @@ int main(){
 
 
 void sieve(long inicio, long fim, long array[]){
-    long limite = fim-inicio;
-    for(long i=0; i<=limite; i++){
+    long limite = fim-inicio+1;
+    for(long i=0; i<limite; i++){
         array[i] = inicio+i;
     }
 
-    for(long i=0; i<=limite; i++){
-        for(long j=2; j*j < limite; j++){
+    for(long i=0; i<limite; i++){
+        for(long j=2; j*j <= limite; j++){
             if(array[i] % j == 0 && array[i] != j){
                 array[i] = 0;
                 break;
@@ -28,8 +28,8 @@ void sieve(long inicio, long fim, long array[]){
 
     if(array[0] == 1) array[0] = 0; // Verificar se começamos no 1 (e retirar este elemento)
 
-    for(long i=0; i<=limite; i++) {
-        if(array[i]) printf("%ld ", array[i]);
+    for(long i=0; i<limite; i++) {
+        if(array[i]) printf("%ld\t", array[i]);
     }
     printf("\n");
 

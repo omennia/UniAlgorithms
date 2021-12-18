e3322dd704da0a5a1909aa23cfe94f5aa1ec82fb
omennia <65104945+omennia@users.noreply.github.com>
2021-12-18 23:01:58 +0000

    Create count_inversions.cpp
    Using merge sort to count inversions.

---
+++ count_inversions.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+void print(int arr[], int sz){
+    for(int i=0; i<sz; ++i) cout << arr[i] << ' ';
+    cout << '\n';
+}
+
+int cnt_inversions = 0;
+
+void merge(int arr[], int lo, int mid, int hi){
+    int aux[hi-lo + 1];
+    int p1 = lo, p2 = mid + 1, idx = 0;
+    while(p1 <= mid && p2 <= hi){
+        if(arr[p1] < arr[p2]){
+            aux[idx++] = arr[p1++];
+        }
+        else{
+            cnt_inversions += mid - p1 + 1;
+            aux[idx++] = arr[p2++];
+        }
+    }
+    while(p1 <= mid) aux[idx++] = arr[p1++];
+    while(p2 <= mid) aux[idx++] = arr[p2++];
+    for(int i=0; i<idx; ++i) arr[lo+i] = aux[i];
+}
+
+void mergesort(int arr[], int lo, int hi){
+    if(lo >= hi) return;
+    int mid = lo + (hi - lo) / 2;
+    mergesort(arr, lo, mid);
+    mergesort(arr, mid+1, hi);
+    merge(arr, lo, mid, hi);
+}
+
+
+
+signed main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+
+    int arr[] = {3, 5, 4, 1, 2};
+    int sz = sizeof(arr)/sizeof(arr[0]);
+    cout << "size: " << sz << '\n';
+    print(arr, sz);
+    mergesort(arr, 0, sz);
+    print(arr, sz);
+    cout << "inversions: " << cnt_inversions << '\n';
+}

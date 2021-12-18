d74ca7398944f0b134ce19d33bead9fa1fd9e823
omennia <65104945+omennia@users.noreply.github.com>
2021-12-18 23:04:59 +0000

    Update count_inversions.cpp

--- count_inversions.cpp
+++ count_inversions.cpp
@@ -16,8 +16,8 @@ void merge(int arr[], int lo, int mid, int hi){
             aux[idx++] = arr[p1++];
         }
         else{
-            cnt_inversions += mid - p1 + 1;
-            aux[idx++] = arr[p2++];
+            cnt_inversions += mid - p1 + 1;  // Count the inversions: To any element we choose from the "right" side, we have 
+            aux[idx++] = arr[p2++];          //                       to inversions for that number equal to the counting from p1 to mid. // see slides de AMD aula 11 to clarify 
         }
     }
     while(p1 <= mid) aux[idx++] = arr[p1++];
@@ -33,12 +33,10 @@ void mergesort(int arr[], int lo, int hi){
     merge(arr, lo, mid, hi);
 }
 
-
-
 signed main(){
     ios::sync_with_stdio(0); cin.tie(0);
 
-    int arr[] = {3, 5, 4, 1, 2};
+    int arr[] = {3, 5, 4, 1, 2}; // Change to whatever array
     int sz = sizeof(arr)/sizeof(arr[0]);
     cout << "size: " << sz << '\n';
     print(arr, sz);
